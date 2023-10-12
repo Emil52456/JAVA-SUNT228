e669e68709e908fde905b3ea8bde4a6b57bb64be
ТАМИК228 <emil.ganeev2017@mail.ru>
2023-10-12 11:34:16 +0500

    МОЙ ПЕРВЫЙ КаММИТ

---
+++ src/main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        for( int s = 0; s<=2023;s=s+79)
+        {
+        }
+        for( int g = 1896; g<=2075; g=g+79){
+            System.out.println(g);
+        }
+
+}}
